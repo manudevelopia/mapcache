c2556e0965314c4bcf7427cbb4f92bc915d3313a
manudevelopia <manu@developia.info>
2023-02-13 10:09:50 +0100

    improve cache examples

--- app/src/main/java/info/developia/app/App.java
+++ app/src/main/java/info/developia/app/App.java
@@ -9,14 +9,13 @@ import java.util.Map;
 import java.util.function.Supplier;
 
 public class App {
+    private static final Supplier<Map<String, String>> filler = () -> Map.of(
+            "key1", "value1",
+            "key2", "value2",
+            "key3", "value3"
+    );
 
     public static void main(String[] args) throws InterruptedException {
-        Supplier<Map<String, String>> filler = () -> Map.of(
-                "key1", "value1",
-                "key2", "value2",
-                "key3", "value3"
-        );
-
         Cache<String, String> cache = Macache.basic();
         cache.put("key", "value");
         cache.get("key");
@@ -26,6 +25,15 @@ public class App {
         cacheFilledOnce.put("key4", "value4");
         cacheFilledOnce.get("key4");
         cacheFilledOnce.size();
+        try (Cache<String, String> cacheExpire = Macache.expireIn(Duration.ofMinutes(1))) {
+            filler.get().forEach(cacheExpire::put);
+            int counter = 50;
+            while (--counter > 0) {
+                Thread.sleep(1000);
+                var print = cacheExpire.get("key1");
+                System.out.println("counter %d time %s value %s".formatted(counter, LocalDateTime.now(), print));
+            }
+        }
 
 //        var cache = new Macache<String, String>();
 //        cache.put("key", "value");
@@ -38,12 +46,5 @@ public class App {
 //
 //        var cacheWithFillerSchedule = new Macache<>(filler, Duration.ofSeconds(12));
 //        cacheWithFillerSchedule.size();
-        Cache<String, String> cacheExpire = Macache.expireIn(Duration.ofMinutes(1));
-        filler.get().forEach(cacheExpire::put);
-        while (true) {
-            Thread.sleep(1000);
-            var print = cacheExpire.get("key1");
-            System.out.println("time %s value %s".formatted(LocalDateTime.now(), print));
-        }
     }
 }
